5037f03a39eebac46b4ceb50616db53a10fca6b8
Milayn <millayunita07@gmail.com>
2022-12-25 02:40:15 +0700

    flutter_sqflite

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,15 +1,31 @@
 class Item {
-  late int _id;
-  late String name;
-  late int price;
-  int get id => _id;
-  Item({required this.name, required this.price});
+  int? _id;
+  String? _name;
+  int? _price;
+
+  int get id => _id!;
+
+  String get name => this._name!;
+  set name(String value) => this._name = value;
+
+  get price => this._price!;
+  set price(value) => this._price = value;
+
+// konstruktor versi 1
+  Item(this._name, this._price);
+
+// konstruktor versi 2: konversi dari Map ke Item
   Item.fromMap(Map<String, dynamic> map) {
-    _id = map['id'];
-    name = map['name'];
-    price = map['price'];
+    this._id = map['id'];
+    this._name = map['name'];
+    this._price = map['price'];
   }
+// konversi dari Item ke Map
   Map<String, dynamic> toMap() {
-    return {'id': _id, 'name': name, 'price': price};
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map['id'] = this._id;
+    map['name'] = name;
+    map['price'] = price;
+    return map;
   }
 }
