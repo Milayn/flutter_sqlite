c2b2f395f3093f2c131702cf14b5d1a734ae2d92
Milayn <millayunita07@gmail.com>
2022-12-26 12:23:45 +0700

    update flutter sqflite

--- lib/entryForm.dart
+++ lib/entryForm.dart
@@ -1,102 +1,127 @@
 import 'package:flutter/material.dart';
-import '../models/item.dart';
+import 'package:flutter_sqlite/models/item.dart';
 
 class EntryForm extends StatefulWidget {
-  Item? item;
-
-  EntryForm({Key? key, required this.item}) : super(key: key);
-
+  final Item item;
+  const EntryForm(this.item, {Key key}) : super(key: key);
   @override
-  State<EntryForm> createState() => _EntryFormState(this.item);
+  EntryFormState createState() => EntryFormState(item);
 }
 
-class _EntryFormState extends State<EntryForm> {
-  Item? item;
-  _EntryFormState(this.item);
-
+class EntryFormState extends State<EntryForm> {
+  Item item;
+  EntryFormState(this.item);
   TextEditingController nameController = TextEditingController();
   TextEditingController priceController = TextEditingController();
+  TextEditingController stockController = TextEditingController();
+  TextEditingController kodeBarangController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    //kondisi
     if (item != null) {
-      nameController.text = item!.name;
-      priceController.text = item!.price.toString();
+      nameController.text = item.name;
+      priceController.text = item.price.toString();
+      stockController.text = item.stock.toString();
+      kodeBarangController.text = item.kodeBarang;
     }
     return Scaffold(
-        appBar: AppBar(
-            title: item == null ? Text('Tambah') : Text('Ubah'),
-            leading: Icon(Icons.keyboard_arrow_left)),
-        body: Padding(
-          padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
-          child: ListView(
-            children: [
-              // nama
-              Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                child: TextField(
-                  controller: nameController,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    labelText: 'Nama Barang',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
+      appBar: AppBar(
+        title: item == null ? const Text('Tambah') : const Text('Ubah'),
+        leading: const Icon(Icons.keyboard_arrow_left),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Nama Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
                   ),
-                  onChanged: (value) {
-                    //
-                  },
                 ),
+                onChanged: (value) {},
               ),
-
-              // harga
-              Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                child: TextField(
-                  controller: priceController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: 'Harga',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: priceController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Harga',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
                   ),
-                  onChanged: (value) {},
                 ),
+                onChanged: (value) {},
               ),
-
-              Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: ElevatedButton(
-                        child: const Text(
-                          "Save",
-                          textScaleFactor: 1.5,
-                        ),
-                        onPressed: () {
-                          if (item == null) {
-                            // Tambah Data
-                            item = Item(
-                              nameController.text,
-                              int.parse(priceController.text),
-                            );
-                          } else {
-                            item!.name = nameController.text;
-                            item!.price = int.parse(priceController.text);
-                          }
-                          Navigator.pop(context, item);
-                        },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: stockController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Stok',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: TextField(
+                controller: kodeBarangController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Kode Barang',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      child: const Text(
+                        'Save',
+                        textScaleFactor: 1.5,
                       ),
+                      onPressed: () {
+                        if (item == null) {
+                          item = Item(
+                            nameController.text,
+                            int.parse(priceController.text),
+                            int.parse(stockController.text),
+                            kodeBarangController.text,
+                          );
+                        } else {
+                          item?.name = nameController.text;
+                          item?.price = int.parse(priceController.text);
+                          item?.stock = int.parse(stockController.text);
+                          item?.kodeBarang = kodeBarangController.text;
+                        }
+                        Navigator.pop(context, item);
+                      },
                     ),
-                    Container(
-                      width: 5.0,
-                    ),
-                    //tombol batal
-                    Expanded(
-                        child: ElevatedButton(
+                  ),
+                  Container(
+                    width: 5.0,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
                       child: const Text(
                         'Cancel',
                         textScaleFactor: 1.5,
@@ -104,12 +129,14 @@ class _EntryFormState extends State<EntryForm> {
                       onPressed: () {
                         Navigator.pop(context);
                       },
-                    ))
-                  ],
-                ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,68 +1,72 @@
 import 'package:flutter/material.dart';
-import '../helper/dbhelper.dart';
-import '../entryform.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
-import 'models/item.dart';
+import 'package:flutter_sqlite/models/item.dart';
+import 'package:flutter_sqlite/entryForm.dart';
+import 'package:flutter_sqlite/helper/dbhelper.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({Key key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  HomeState createState() => HomeState();
 }
 
-class _HomeState extends State<Home> {
+class HomeState extends State<Home> {
   DbHelper dbHelper = DbHelper();
   int count = 0;
-  List<Item>? itemList;
-
+  List<Item> itemList;
   @override
   Widget build(BuildContext context) {
-    if (itemList == null) {
-      itemList = <Item>[];
-    }
+    updateListView();
+
+    itemList ??= <Item>[];
     return Scaffold(
       appBar: AppBar(
-        title: Text('Daftar Item'),
+        title: const Text('Daftar Item - Mila Yunita'),
       ),
-      body: Column(children: [
-        Expanded(
-          child: createListView(),
-        ),
-        Container(
-          alignment: Alignment.bottomCenter,
-          child: SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              child: Text("Tambah Item"),
-              onPressed: () async {
-                var item = await navigateToEntryForm(context, null);
-                if (item != null) {
-                  //TODO 2 Panggil Fungsi untuk Insert ke DB
-                  int result = await dbHelper.insert(item);
-                  if (result > 0) {
-                    updateListView();
+      body: Column(
+        children: [
+          Expanded(
+            child: createListView(),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                child: const Text("Tambah Item"),
+                onPressed: () async {
+                  var item = await navigateToEntryForm(context, null);
+                  if (item != null) {
+                    int result = await dbHelper.insert(item);
+                    if (result > 0) {
+                      updateListView();
+                    }
                   }
-                }
-              },
+                },
+              ),
             ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
-  
-  Future<Item?> navigateToEntryForm(BuildContext context, Item? item) async {
-    var result = await Navigator.push(context,
-        MaterialPageRoute(builder: (BuildContext context) {
-      return EntryForm(item: item);
-    }));
+
+  Future<Item> navigateToEntryForm(BuildContext context, Item item) async {
+    var result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return EntryForm(item);
+        },
+      ),
+    );
     return result;
   }
 
   ListView createListView() {
-    TextStyle? textStyle = Theme.of(context).textTheme.headline5;
+    TextStyle textStyle = Theme.of(context).textTheme.headline5;
     return ListView.builder(
       itemCount: count,
       itemBuilder: (BuildContext context, int index) {
@@ -70,43 +74,39 @@ class _HomeState extends State<Home> {
           color: Colors.white,
           elevation: 2.0,
           child: ListTile(
+            isThreeLine: true,
             leading: const CircleAvatar(
               backgroundColor: Colors.red,
-              child: Icon(Icons.ad_units),
+              child: Icon(Icons.people_alt),
             ),
             title: Text(
-              this.itemList![index].name,
+              itemList[index].name,
               style: textStyle,
             ),
-            // subtitle: Text(this.itemList![index].price.toString()),
-            subtitle: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(this.itemList![index].price.toString()),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Harga : " + itemList[index].price.toString()),
+                const SizedBox(
+                  height: 5.0,
                 ),
+                Text("Stok : " + itemList[index].stock.toString()),
+                Text("" + itemList[index].kodeBarang.toString()),
               ],
             ),
+            // 3.TODO : delete by id
             trailing: GestureDetector(
-              child: Icon(Icons.delete),
+              child: const Icon(Icons.delete),
               onTap: () async {
-                //TODO 3 Panggil Fungsi untuk Delete dari DB berdasarkan Item
-                int result = await dbHelper.delete(this.itemList![index].id);
-                if (result > 0) {
-                  updateListView();
-                }
+                dbHelper.delete(itemList[index].id);
+                updateListView();
               },
             ),
+            //4. TODO: update by id
             onTap: () async {
-              var item =
-                  await navigateToEntryForm(context, this.itemList![index]);
-              //TODO 4 Panggil Fungsi untuk Edit data
-              if (item != null) {
-                //TODO 5 Panggil Fungsi untuk Update data
-                int result = await dbHelper.update(item);
-                if (result > 0) {
-                  updateListView();
-                }
-              }
+              var item = await navigateToEntryForm(context, itemList[index]);
+              dbHelper.update(item);
+              updateListView();
             },
           ),
         );
@@ -114,17 +114,22 @@ class _HomeState extends State<Home> {
     );
   }
 
-//update List item
   void updateListView() {
     final Future<Database> dbFuture = dbHelper.initDb();
-    dbFuture.then((database) {
-      Future<List<Item>> itemListFuture = dbHelper.getItemList();
-      itemListFuture.then((itemList) {
-        setState(() {
-          this.itemList = itemList;
-          this.count = itemList.length;
-        });
-      });
-    });
+    dbFuture.then(
+      (database) {
+        Future<List<Item>> itemListFuture = dbHelper.getItemList();
+        itemListFuture.then(
+          (itemList) {
+            setState(
+              () {
+                this.itemList = itemList;
+                count = itemList.length;
+              },
+            );
+          },
+        );
+      },
+    );
   }
 }
